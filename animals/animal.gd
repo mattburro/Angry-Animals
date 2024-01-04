dcd5af63c774ca223fe7b4f0415cee5109ba7a95
Matt Burroughs <matt.burro@gmail.com>
2024-01-03 20:53:55 -0500

    Respawn animal when it exits screen

--- animals/animal.gd
+++ animals/animal.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+var dead: bool = false
+
 func _physics_process(delta):
 	update_debug_label()
 
@@ -7,3 +9,14 @@ func update_debug_label() -> void:
 	var info_str = "g_pos: %s\n" % Utils.vec2_to_str(global_position)
 	info_str += "angular v: %0.1f\nlinear v: %s" % [angular_velocity, Utils.vec2_to_str(linear_velocity)]
 	SignalManager.on_update_debug_label.emit(info_str)
+
+func die() -> void:
+	if dead:
+		return
+	
+	dead = true
+	SignalManager.on_animal_died.emit()
+	queue_free()
+
+func on_screen_exited():
+	die()
